e8b35c5eb43f1b32dc52660c02bae74b83d3e45d
Tanisha Gupta <tanisha11072005@gmail.com>
2026-09-03 12:57:03 +0530

    Update counter_tb.v

--- RTL and Simulation/counter_tb.v
+++ RTL and Simulation/counter_tb.v
@@ -1,13 +1,21 @@
 module counter_test;
-reg clk, rst;
-wire [7:0] count;
-initial 
-begin
-clk=0;
-rst=0;#10;
-rst=1;
-#200 $stop;
-end
-counter counter1(clk,rst, count);
-always #5 clk=~clk;
+  reg clk, rst;
+  wire [7:0] count;
+
+  initial begin
+    clk = 0;
+    rst = 0;     // Initial state (0)
+    #10;
+    rst = 1;     // Transition 1: 0 -> 1
+    #150;
+    rst = 0;     // Transition 2: 1 -> 0 (completes toggle coverage)
+    #20;
+    rst = 1;     // Release reset to resume counting
+    #20;
+    $finish;
+  end
+
+  counter counter1(clk, rst, count);
+
+  always #5 clk = ~clk;
 endmodule
